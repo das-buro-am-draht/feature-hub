ec7dd92f8219362baae6dba745c418e2b958d36b
Niels Garve <info@nielsgarve.com>
2020-02-25 15:56:26 +0100

    feat(module-loader-commonjs): add proxy support

--- packages/module-loader-commonjs/src/index.ts
+++ packages/module-loader-commonjs/src/index.ts
@@ -1,5 +1,7 @@
 import {ModuleLoader} from '@feature-hub/core';
-import fetch from 'node-fetch';
+import {Agent} from 'http';
+import fetch, {RequestInit} from 'node-fetch';
+import ProxyAgent from 'proxy-agent';
 
 export interface Externals {
   readonly [externalName: string]: unknown;
@@ -9,7 +11,13 @@ export function createCommonJsModuleLoader(
   externals: Externals = {}
 ): ModuleLoader {
   return async (url: string): Promise<unknown> => {
-    const response = await fetch(url);
+    const requestInit: RequestInit = {};
+
+    if (process.env.HTTP_PROXY) {
+      requestInit.agent = new ProxyAgent(process.env.HTTP_PROXY) as unknown as Agent;
+    }
+
+    const response = await fetch(url, requestInit);
     const source = await response.text();
     const mod = {exports: {}};
 
